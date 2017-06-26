730a698174e645002ca09495ea05d75374623778
Samuel Bancal <Samuel.Bancal@epfl.ch>
2017-06-26 13:19:07 +0200

    2017-06-26 +Character component

---
+++ applications_SB/starbase/src/components/Character.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+  <div>
+    {{character}}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      character: {}
+    }
+  },
+  methods: {
+    fetchCharacter() {
+      fetch('http://swapi.co/api/people/1', {
+        method: 'GET'
+      })
+      .then(response => response.json())
+      .then(json => this.character = json)
+    }
+  },
+  created() {
+    console.log('Created')
+    this.fetchCharacter()
+  }
+}
+</script>
+
+<style lang="css">
+</style>
