1312f2af47e429646efa83140213ccc67c724702
Samuel Bancal <Samuel.Bancal@epfl.ch>
2017-06-26 14:09:32 +0200

    +Nice rendering of Characters

--- applications_SB/starbase/src/components/Character.vue
+++ applications_SB/starbase/src/components/Character.vue
@@ -1,6 +1,14 @@
 <template lang="html">
-  <div @click="switchCharacter">
-    {{character}}
+  <div class="col-md-4" @click="switchCharacter">
+    <div class="character-card">
+      <div class="character-block">
+        <h4 class="card-title">{{character.name}}</h4>
+        <p class="card-text">Height: {{getHeight}}</p>
+        <p class="card-text">Mass: {{getMass}}</p>
+        <p class="card-text">Hair_color: {{character.hair_color}}</p>
+        <p class="card-text">Eye_color: {{character.eye_color}}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -23,6 +31,22 @@ export default {
     switchCharacter() {
       let random_id = Math.floor(Math.random() * 83) + 1
       this.fetchCharacter(random_id)
+    }
+  },
+  computed: {
+    getHeight() {
+      if (isNaN(Number(this.character.height))) {
+        return this.character.height
+      } else {
+        return this.character.height + 'cm'
+      }
+    },
+    getMass() {
+      if (isNaN(Number(this.character.mass))) {
+        return this.character.mass
+      } else {
+        return this.character.mass + 'kg'
+      }
     }
   },
   created() {
