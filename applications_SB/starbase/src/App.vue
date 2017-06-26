088ed63919f23a3bee6abd3c78b9c36b7df09a8a
Samuel Bancal <Samuel.Bancal@epfl.ch>
2017-06-26 12:47:01 +0200

    2017-06-26 +Start to fetch JSON data from Star Wars API

--- applications_SB/starbase/src/App.vue
+++ applications_SB/starbase/src/App.vue
@@ -1,6 +1,7 @@
 <template lang="html">
   <div id="app">
     <h3>{{title}}</h3>
+    <button class="btn btn-primary" @click="fetchCharacter">Test Fetch Character</button>
   </div>
 </template>
 
@@ -10,6 +11,15 @@ export default {
   data() {
     return {
       title: 'Generate Your Team'
+    }
+  },
+  methods:{
+    fetchCharacter() {
+      fetch('http://swapi.co/api/people', {
+        method: 'GET'
+      })
+      .then(response => response.json())
+      .then(json => console.log(json))
     }
   }
 }
