4878ac3187516187423dca22b517fb7218b73fa8
Samuel Bancal <Samuel.Bancal@epfl.ch>
2017-06-29 15:20:57 +0200

    +link to vue-routes and watch for changes

--- applications_SB/starbase-router/src/App.vue
+++ applications_SB/starbase-router/src/App.vue
@@ -1,6 +1,10 @@
 <template lang="html">
   <div id="app">
     <h3>Starbase router</h3>
+    <h4>
+      <router-link class="link" to="/data/people">Meet the Characters</router-link> |
+      <router-link class="link" to="/data/planets">Explore the Planets</router-link>
+    </h4>
     <router-view/>
   </div>
 </template>
