ff5581e4fa06a0b7da62644459b95d43f502489e
Samuel Bancal <Samuel.Bancal@epfl.ch>
2017-06-29 15:33:55 +0200

    fetch people & planets from swapi.co

--- applications_SB/starbase-router/src/components/Data.vue
+++ applications_SB/starbase-router/src/components/Data.vue
@@ -8,16 +8,32 @@
 export default {
   data() {
     return {
-      type: this.$route.params.type
+      type: this.$route.params.type,
+      items: []
     }
   },
   watch: {
-    '$route': 'change'
+    '$route': 'fetchItems'
   },
   methods: {
-    change() {
+    fetchItems() {
       this.type = this.$route.params.type
+      this.items = []
+      let initial_ids = [1, 13, 14]
+
+      for (let i in initial_ids) {
+        let id = initial_ids[i]
+        console.log('id', id)
+        fetch(`http://swapi.co/api/${this.type}/${id}`, {
+          method: 'GET'
+        })
+        .then(response => response.json())
+        .then(json => this.items.push(json))
+      }
     }
+  },
+  created() {
+    this.fetchItems()
   }
 }
 </script>
