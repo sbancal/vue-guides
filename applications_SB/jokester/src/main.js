b08693e139b8de496c9f87da7c442a32b7f7e56b
Samuel Bancal <Samuel.Bancal@epfl.ch>
2017-06-30 15:34:12 +0200

    +vuex 1st mutation

--- applications_SB/jokester/src/main.js
+++ applications_SB/jokester/src/main.js
@@ -3,6 +3,11 @@ import App from './components/App.vue'
 
 import store from './store'
 
+store.commit('INIT_JOKES', [
+  {test: 'test_joke'},
+  {test2: 'test_joke_2'}
+])
+
 new Vue({
   el: '#app',
   store,
