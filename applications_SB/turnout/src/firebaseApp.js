85fadf124e21c76e776d1e534d5914373714eb40
Samuel Bancal <Samuel.Bancal@epfl.ch>
2017-07-04 14:10:43 +0200

    +linked to firebase.google.com instance "tuto-turnout"
    tuto-turnout was created on firebase.google.com with Authentication >
    Sign-in method > Email/Password : Enabled

---
+++ applications_SB/turnout/src/firebaseApp.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+const config = {
+  apiKey: "AIzaSyDJzlnGHBqY2Hw7W6bpZ3889wvai7hMFPg",
+  authDomain: "tuto-turnout.firebaseapp.com",
+  databaseURL: "https://tuto-turnout.firebaseio.com",
+  projectId: "tuto-turnout",
+  storageBucket: "",
+  messagingSenderId: "71527330828"
+}
+
+export const firebaseApp = firebase.initializeApp(config)
