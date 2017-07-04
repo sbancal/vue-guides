8c1bd0b32421798f68e0fef0c1a4c1e9f8d0c534
Samuel Bancal <Samuel.Bancal@epfl.ch>
2017-07-04 13:19:51 +0200

    Base setup to start dev of turnout app

---
+++ applications_SB/turnout/src/components/App.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div id="app">
+    App component
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="css">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2C3350;
+  margin-top: 60px;
+}
+</style>
