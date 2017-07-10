4fc5fb1fc847bfb40aeda7eae8524823406eb7bb
Samuel Bancal <Samuel.Bancal@epfl.ch>
2017-07-10 13:09:54 +0200

    +Signup component
    + router-link between /signin and /signup

--- applications_SB/turnout/src/components/Signup.vue
+++ applications_SB/turnout/src/components/Signup.vue
@@ -1,6 +1,6 @@
 <template lang="html">
   <div class="form-inline">
-    <h3>Sign in</h3>
+    <h3>Sign Up</h3>
     <div class="form-group">
       <input
         type="text"
@@ -14,12 +14,12 @@
         class="form-control"
         v-model="password"
       >
-      <button class="btn btn-primary" @click="signIn">Sign In</button>
+      <button class="btn btn-primary" @click="signUp">Sign Up</button>
     </div>
     <br>
     <p>{{error.message}}</p>
     <br>
-    <router-link to="/signup">Don't have an account? Sign Up!</router-link>
+    <router-link to="/signin">Already have an account? Sign In!</router-link>
   </div>
 </template>
 
@@ -37,8 +37,8 @@ export default {
     }
   },
   methods: {
-    signIn() {
-      firebaseApp.auth().signInWithEmailAndPassword(this.email, this.password)
+    signUp() {
+      firebaseApp.auth().createUserWithEmailAndPassword(this.email, this.password)
       .catch(error => {
         this.error = error
       })
