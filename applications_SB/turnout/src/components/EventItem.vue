9a704cfacbf50c1f01073180338d13e60ed41784
Samuel Bancal <Samuel.Bancal@epfl.ch>
2017-07-11 14:52:59 +0200

    +EventItem.vue component

---
+++ applications_SB/turnout/src/components/EventItem.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+  <div class="col-md-4">
+    <div class="event-card">
+      <h4 class="card-title">{{event.title}}</h4>
+      <p class="card-text">{{event.description}}</p>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Date: {{event.date}}</li>
+        <li class="list-group-item">Location: {{event.location}}</li>
+        <li class="list-group-item">Host: {{event.email}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['event']
+}
+</script>
+
+<style lang="css">
+.event-card {
+  border: 2px solid #2C3E50;
+  margin-top: 10px;
+  border-radius: 4px;
+}
+</style>
