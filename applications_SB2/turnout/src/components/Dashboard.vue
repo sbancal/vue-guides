a2695dfd80149d12b09ed1cb1a001a391023c0e8
Samuel Bancal <Samuel.Bancal@epfl.ch>
2017-07-14 17:17:25 +0200

    +firebase Signout

--- applications_SB2/turnout/src/components/Dashboard.vue
+++ applications_SB2/turnout/src/components/Dashboard.vue
@@ -1,11 +1,19 @@
 <template lang="html">
   <div class="">
     <h1>Dashboard</h1>
+    <button class="btn btn-danger btn-sm" @click="signOut">Sign Out</button>
   </div>
 </template>
 
 <script>
+import { firebaseApp } from '../firebaseApp'
+
 export default {
+  methods: {
+    signOut() {
+      firebaseApp.auth().signOut()
+    }
+  }
 }
 </script>
 
